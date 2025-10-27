8bf681efee3ec126447d7bfe1c2cff350e651119
Giovanni Azevedo <giovanniadev@gmail.com>
2025-10-27 14:18:30 -0300

    seguem arquivos para commit

--- RestWithASPNET10Erudio_ScaffoldViaTerminal/RestWithASPNET10Erudio/RestWithASPNET10Erudio/Model/Greeting.cs
+++
@@ -1,9 +0,0 @@
-namespace RestWithASPNET10Erudio.Model;
-
-public class Greeting
-{
-    public long Id { get; init; }
-    public string Content { get; init; }
-
-    public Greeting(long id, string content) => (Id, Content) = (id, content);
-}
